713e744d367f660f4d4c4290add742240805daa5
user.name "Ansarul Haq <mdansarul786940@gmail.com>
2024-06-13 07:14:27 +0545

    Resolved the naming conflit in distance constructor

--- Basic/addTwoDistanceUsingOperatorOverloadingAndFriendFunction.cpp
+++ Basic/addTwoDistanceUsingOperatorOverloadingAndFriendFunction.cpp
@@ -1,32 +1,33 @@
-//it's not working
-//may be the implementation of polymorphism is wromg
-//#issues
+// changed class name to upper case
 #include <iostream>
 using namespace std;
-class distance{
+class Distance{
 private:
     int m, km;
 public:
-    distance(int m = 0, int km = 0){
-        m = m;
-        km = km;
+//naming comflict
+//ambiguous name of attribute
+    Distance(int meter = 0, int kilometer = 0){
+        m = meter;
+        km = kilometer;
     }
     void display(){
         cout << km << "km" << m << "m" << endl;
     }
-    friend distance operator +(distance d1, distance d2);
+    friend Distance operator +(Distance d1, Distance d2);
 };
-distance operator +(distance d1, distance d2){
-    distance d;
-    d.m = (d1.m + d2.m) % 1000;
-    d.km = (d1.m + d2.m) / 1000 + (d1.km + d2.km);
+Distance operator +(Distance d1, Distance d2){
+    Distance d;
+    d.m = (d1.m + d2.m)%1000;
+    d.km = ((d1.m + d2.m)/1000) + (d1.km + d2.km);
     return d;
 }
 int main(){
-    distance d1(2, 300);
-    distance d2(3, 800);
-    distance d3;
-    d3 = d1+d2; //line of error *fix
+    //correct order of arguement passing
+    Distance d1(300, 2);
+    Distance d2(800, 3);
+    Distance d3;
+    d3 = d1+d2;
     d3.display();
     return 0;
 }
