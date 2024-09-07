dde449d9663773b8bc57dff609ad3285ac87aaa6
user.name "Ansarul Haq <mdansarul786940@gmail.com>
2024-09-07 20:05:47 +0545

    use of vector in stl

--- Basic/vectorType.cpp
+++ Basic/vectorType.cpp
@@ -1,5 +1,19 @@
 // vector type
 #include<iostream>
+#include<vector>
 using namespace std;
-int mai(){
+int main(){
+    vector<int> vec;
+    int size, input;
+    cout<<"Enter number of inputs:"<<endl;
+    cin>>size;
+    for(int i=0; i<size; i++){
+        cout<<"Enter number "<<i+1<<":"<<endl;
+        cin>>input;
+        vec.push_back(input);
+    }
+    for(int i=0; i<size; i++){
+        cout<<vec[i]<<" ";
+    }
+    return 0;
 }
