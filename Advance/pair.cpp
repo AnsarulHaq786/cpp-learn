f0db1046db5eafef760016c61ca7141ab982e363
user.name "Ansarul Haq <mdansarul786940@gmail.com>
2024-11-11 19:14:05 +0545

    pair in c++

---
+++ Advance/pair.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+using namespace std;
+
+int main() {
+    pair<int, int> p(3, 3);
+    cout<<p.first<<" "<<p.second;
+    return 0;
+}
