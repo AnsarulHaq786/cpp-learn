0f4d816f6cc57fbb003e053ad791874e137bc1fb
user.name "Ansarul Haq <mdansarul786940@gmail.com>
2024-11-12 08:33:25 +0545

    next permutation in c++ string

---
+++ Advance/algorithm.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n=7;
+    string s = "123"; // has have to be sorted
+    do{
+        cout<<s<<endl;
+    }while(next_permutation(s.begin(), s.end()));
+    return 0;
+}
